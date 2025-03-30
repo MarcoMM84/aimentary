af4429690da3daa345699f7d813cec4bf7e8e628
MarcoMM84 <melonimarco84@gmail.com>
2025-03-30 19:38:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-/import React from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Auth from './components/Auth';
 import Home from './components/Home';
@@ -35,4 +35,3 @@ function App() {
 }
 
 export default App;
-/ src/App.js - codice React reale incluso qui

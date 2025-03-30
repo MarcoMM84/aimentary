e7c17c7a5f06c9db74f5ad6773fce857753bb21e
MarcoMM84 <melonimarco84@gmail.com>
2025-03-30 19:36:56 +0200

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,1 +1,16 @@
-// src/firebase/firebase.js - codice React reale incluso qui
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_XZdPBFfJ1hmFTJ40RHGUq6n66X4jhVk",
+  authDomain: "aimentary.firebaseapp.com",
+  projectId: "aimentary",
+  storageBucket: "aimentary.firebasestorage.app",
+  messagingSenderId: "619927155053",
+  appId: "1:619927155053:web:77bd176255e2e365932a64"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);

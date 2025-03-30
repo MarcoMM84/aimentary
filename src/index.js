0fa6c68afcebe97d7607dc7ddbc4df86de482679
MarcoMM84 <melonimarco84@gmail.com>
2025-03-30 19:25:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-// src/index.js - codice React reale incluso qui
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Puoi usare Tailwind qui se lo configuri
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+ src/index.js - codice React reale incluso qui
